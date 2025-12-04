b3f9ac8b3e123296170d6863c58e8ad0951bb486
Preto T.I <epilif.cire@gmail.com>
2025-12-04 03:15:05 -0300

    Change server port from 3000 to 3001

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Pasta principal de documentos
 const pastaDocumentos = path.join(__dirname, "documentos");
